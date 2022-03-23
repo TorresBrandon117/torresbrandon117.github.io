5597c5ddc233d8527d09316ab5c7542e39380aa6
Armand Thomas <armandthomas@Armands-Air.fios-router.home>
2022-03-22 22:00:23 -0400

    added personal image to .js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './BrandonTorres.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <code>My name is Brandon Torres and I'm going to master React!</code>
+        </p> 
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/TorresBrandon117"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me out!
         </a>
       </header>
     </div>
